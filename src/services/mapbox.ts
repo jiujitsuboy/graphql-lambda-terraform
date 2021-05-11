75cf2290ee586f129e231fd2fc9fdb748cf606ed
jose.nino <jose.nino@geniussportsmedia.com>
2021-05-11 01:38:19 -0500

    add mapbox service

---
+++ src/services/mapbox.ts
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch'
+
+class MapBoxService {
+  static url: String =
+    'https://api.mapbox.com/geocoding/v5/mapbox.places/[address].json?access_token=pk.eyJ1Ijoiaml1aml0c3Vib3kiLCJhIjoiY2tvZ21pZGRmMGdrejJwbngzNTRla3kzYiJ9.PwbAnNWHysiVkoOu8Wf2pw'
+
+  static async getCoordinateFromAddress(address: any): Promise<String> {
+    const endpoint = this.url.replace("[address]", encodeURIComponent(address));
+    const resp = await fetch(endpoint);
+    const json = await resp.json();
+    return json;
+  }
+}
+
+export default MapBoxService
